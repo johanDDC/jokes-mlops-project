0a42fbdb718666786928c3a19215c7762163dc71
johanddc <ivan.peshekhonov@gmail.com>
2023-12-25 17:15:22 +0300

    Set correct mlflow path

--- configs/mlflow_cfg.py
+++ configs/mlflow_cfg.py
@@ -4,5 +4,5 @@ from dataclasses import dataclass
 @dataclass
 class MLFlowConfig:
     experiment_name: str
-    host: str = "127.0.0.1"
-    port: int = 5000
+    host: str = "128.0.1.1"
+    port: int = 8080
