af0160c5dff6cf76bc141c9cda12c1308bbcd76d
johanddc <ivan.peshekhonov@gmail.com>
2023-12-25 15:58:13 +0300

    Add mlflow tracking to train process

--- train.py
+++ train.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 import dvc.api as dvc
 import hydra
+import mlflow
+import numpy as np
 from configs.experiment_cfg import ExperimentConfig
 from hydra.core.config_store import ConfigStore
 
-from jokes_mlops_project import NGramLanguageModel, Tokenizer, model_to_json
+from jokes_mlops_project import NGramLanguageModel, Tokenizer, model_to_json, perplexity
+from jokes_mlops_project.utils.utils import Timer
 
 
 def preprocess_text(text):
@@ -26,22 +29,43 @@ def main(cfg: ExperimentConfig):
     data_dir = Path("data")
     data_file = "data.txt"
     save_path = Path("chekpoints")
+    timer = Timer()
 
     # get data from dvc
     data: str = dvc.read(str(data_dir / data_file), repo=repository_path, mode="r")
     data = data.strip().replace("<|startoftext|>", "").split("\n\n")
     data = preprocess_text(data)
 
-    # train model
-    tokenizer = Tokenizer(**cfg.tokenizer_cfg)
-    language_model = NGramLanguageModel(tokenizer, **cfg.model_cfg)
-    language_model.fit(data)
+    # setup mlflow
+    mlflow.set_tracking_uri(uri=f"http://{cfg.mlflow_cfg.host}:{cfg.mlflow_cfg.port}")
+    # try:
+    exp_id = mlflow.create_experiment(cfg.mlflow_cfg.experiment_name)
+    mlflow.set_experiment(cfg.mlflow_cfg.experiment_name)
+    with mlflow.start_run(run_name="train", experiment_id=exp_id) as parent_run:
+        run_id = parent_run.info.run_id
+        # with mlflow.start_run(run_name=f"train", experiment_id=exp_id, nested=True) as child_run:
+        params = dict(cfg)
+        params.update({"git_commit_id": "8b5b85b"})
+        mlflow.log_params(params)
+        # train model
+        tokenizer = Tokenizer(**cfg.tokenizer_cfg)
+        language_model = NGramLanguageModel(tokenizer, **cfg.model_cfg)
+        with timer:
+            language_model.fit(data)
+
+        model_train_time = timer.time
+        mlflow.log_metric("model_train_time", model_train_time)
+
+        # count perplexity
+        test_ids = np.random.randint(0, len(data), int(0.01 * len(data)))
+        ppl = perplexity(language_model, data, test_ids)
+        mlflow.log_metric("perplexity", ppl)
 
     # save model
     save_path.mkdir(parents=True, exist_ok=True)
     save_path /= "model.json"
     with save_path.open("w") as outfile:
-        model_to_json(language_model, outfile)
+        model_to_json(language_model, outfile, exp_id=exp_id, run_id=run_id)
 
 
 if __name__ == "__main__":
