de496c0f68d4ff49d09c6e955a28dfdd13e6b9c0
johanddc <ivan.peshekhonov@gmail.com>
2023-12-25 15:56:43 +0300

    Add mlflow cfg and jokes prefixes  to main cfg.

--- configs/experiment_cfg.py
+++ configs/experiment_cfg.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
+from .mlflow_cfg import MLFlowConfig
 from .model_cfg import ModelConfig
 from .tokenizer_cfg import TokenizerConfig
 
@@ -8,3 +10,12 @@ from .tokenizer_cfg import TokenizerConfig
 class ExperimentConfig:
     model_cfg: ModelConfig = ModelConfig(3)
     tokenizer_cfg: TokenizerConfig = TokenizerConfig(50_000, 6, 0.95)
+    mlflow_cfg: MLFlowConfig = MLFlowConfig("ngram_lang_model")
+
+    prefixes: List[str] = field(
+        default_factory=lambda: [
+            "заходит мужик в бар",
+            "купил мужик шляпу",
+            "идёт медведь по лесу",
+        ]
+    )
